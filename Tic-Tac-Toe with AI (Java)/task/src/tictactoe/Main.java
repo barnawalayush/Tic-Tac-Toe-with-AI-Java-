088ffecd590ee1c8164639fe59f0fa722fb81420
Ayush <abarnawal@griddynaamics.com>
2024-01-26 23:50:29 +0530

    stage_2 complete

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -1,79 +1,151 @@
 package tictactoe;
 
+import java.sql.SQLOutput;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    private static int move = 0;
+    private static int move = 0;  // move == 0 --> X turn;   move == 1 --> O turn;
 
     public static void main(String[] args) {
 
-        System.out.print("Enter the cells:");
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
         System.out.println();
 
+        String str = "_________";
         int[][] board = new int[3][3];
 
         update_array(board, str);
 
         show_current_board_status(board);
 
-        System.out.print("Enter the coordinates:");
+        boolean b = false;
+        while(!b){
 
-        takeUserInput(sc, board);
+            if(move == 0){
+                System.out.print("Enter the coordinates:");
+                takeUserInput(sc, board);
+                move = 1-move;
+            }else{
+                System.out.println("Making move level \"easy\"");
+                takeCompuerInput(board);
+                move = 1-move;
+            }
 
-        show_current_board_status(board);
+            show_current_board_status(board);
 
-        checkWinner(board);
+            b = checkWinner(board);
+
+        }
+
+    }
+
+    private static void takeCompuerInput(int[][] board) {
+
+        Random r = new Random();
+
+        while(true){
+
+            int computer_x_coordinate = r.nextInt(3) + 1;
+            int computer_y_coordinate = r.nextInt(3) + 1;
+
+            if(board[computer_x_coordinate-1][computer_y_coordinate-1] != 0){
+                continue;
+            }else{
+                board[computer_x_coordinate-1][computer_y_coordinate-1] = -1;
+                break;
+            }
+        }
 
     }
 
     private static void takeUserInput(Scanner sc, int[][] board) {
 
-        while(true){
-            String str = sc.nextLine();
-//            String str1 = sc.next();
-//            String str2 = sc.next();
-            try{
-                int n1=0, n2=0;
+        int count=0;           // To count number of input tokens
+        boolean check1 = true; // To keep the record of the entered incorrect number to avoid double printing
+        boolean check2 = true; // To keep the record of the entered incorrect word to avoid double printing
+        boolean first = true;  // To instantiate x and y coordinate correspondingly
+        int x_coordinate = -1, y_coordinate = -1;
+        boolean check=true;    // To handle the situation in which if user input like "one"
 
-                String temp = "";
-                for(int i=0; i<str.length(); i++){
-                    if(" ".equals(str.charAt(i))){
-                        i++;
-                        n1 = Integer.parseInt(temp);
-                        temp = "";
-                    }
-                    temp += str.charAt(i);
-                }
-                n2 = Integer.parseInt(temp);
-//                String[] st = str.split(" ");
-//                n1 = Integer.parseInt(st[0]);
-//                n2 = Integer.parseInt(st[1]);
-//                int n1 = Integer.parseInt(str1);
-//                int n2 = Integer.parseInt(str2);
-                if(n1<1 || n1>3 || n2<1 || n2>3){
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    System.out.print("Enter the coordinates:");
-                    continue;
-                }
-                if(board[n1-1][n2-1] != 0){
-                    System.out.println("This cell is occupied! Choose another one!");
-                    System.out.print("Enter the coordinates:");
-                    continue;
-                }
-                if(move == 0)board[n1-1][n2-1] = 1;
-                else board[n1-1][n2-1] = -1;
-                move = 1-move;
-                break;
+        while(count != 2){
+            String str = sc.next();
+            int n = 0;
+
+            if(!check & first){
+                check2 = true;
+                check = true;
+            }
+
+            try{
+                n = Integer.parseInt(str);
             }
             catch (NumberFormatException e){
-                System.out.println("You should enter numbers!");
-                System.out.print("Enter the coordinates:");
-                continue;
+                if(check2){
+                    System.out.println("You should enter numbers!");
+                    System.out.print("Enter the coordinates:");
+                    if(first){
+                        check = false;
+                        check2 = false;
+                    }
+                    first = true;
+                    count=0;
+                    continue;
+                }else{
+                    check = true;
+                    check2 = true;
+                    continue;
+                }
             }
-        }
+
+            if(n<1 || n>3) {
+                if(check1){
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    System.out.print("Enter the coordinates:");
+                    if(first)check1 = false;
+                    first = true;
+                    count=0;
+                    continue;
+                }else{
+                    check1 = true;
+                    continue;
+                }
+
+            }else{
+                if(check1 && check2){
+                    if(first){
+                        first=false;
+                        x_coordinate = n;
+                        count++;
+                    }else{
+                        count++;
+                        y_coordinate = n;
+                        first = true;
+                    }
+                }else{
+                    check1=true;
+                    check2 = true;
+                }
+            }
+
+            if(count==2){
+                if(board[x_coordinate-1][y_coordinate-1] != 0){
+                    System.out.println("This cell is occupied! Choose another one!");
+                    System.out.print("Enter the coordinates:");
+                    count=0;
+                    first = true;
+                    continue;
+                }else{
+                    break;
+                }
+
+            }
+
+        };
+
+        if(move == 0)board[x_coordinate-1][y_coordinate-1] = 1;
+        else board[x_coordinate-1][y_coordinate-1] = -1;
 
     }
 
@@ -96,11 +168,16 @@ public class Main {
     private static void update_array(int[][] board, String str) {
 
         int x1=0,y1=0;
+        int no_of_x = 0, no_of_o = 0;
 
         for(int i=0; i<str.length(); i++){
             if(str.charAt(i) == 'X') {
+                no_of_x++;
                 board[x1][y1] = 1;
-            }else if(str.charAt(i) == 'O') board[x1][y1] = -1;
+            }else if(str.charAt(i) == 'O'){
+                no_of_o++;
+                board[x1][y1] = -1;
+            }
             y1++;
 
             if(i%3==2) {
@@ -110,9 +187,10 @@ public class Main {
             }
         }
 
+        if(no_of_x > no_of_o) move = 1-move;
     }
 
-    private static void checkWinner(int[][] arr) {
+    private static boolean checkWinner(int[][] arr) {
 
         int total_moves=0;
         boolean x_wins=false;
@@ -126,56 +204,95 @@ public class Main {
 
         if(arr[0][0] == 1 && arr[0][1] == 1 && arr[0][2] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][0] == 0 && arr[0][1] == 0 && arr[0][2] == 0){
+        if(arr[0][0] == -1 && arr[0][1] == -1 && arr[0][2] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[1][0] == 1 && arr[1][1] == 1 && arr[1][2] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[1][0] == 0 && arr[1][1] == 0 && arr[1][2] == 0){
+        if(arr[1][0] == -1 && arr[1][1] == -1 && arr[1][2] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[2][0] == 1 && arr[2][1] == 1 && arr[2][2] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[2][0] == 0 && arr[2][1] == 0 && arr[2][2] == 0){
+        if(arr[2][0] == -1 && arr[2][1] == -1 && arr[2][2] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[0][0] == 1 && arr[1][0] == 1 && arr[2][0] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][0] == 0 && arr[1][0] == 0 && arr[2][0] == 0){
+        if(arr[0][0] == -1 && arr[1][0] == -1 && arr[2][0] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[0][1] == 1 && arr[1][1] == 1 && arr[2][1] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][1] == 0 && arr[1][1] == 0 && arr[2][1] == 0){
+        if(arr[0][1] == -1 && arr[1][1] == -1 && arr[2][1] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[0][2] == 1 && arr[1][2] == 1 && arr[2][2] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][2] == 0 && arr[1][2] == 0 && arr[2][2] == 0){
+        if(arr[0][2] == -1 && arr[1][2] == -1 && arr[2][2] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[0][0] == 1 && arr[1][1] == 1 && arr[2][2] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0){
+        if(arr[0][0] == -1 && arr[1][1] == -1 && arr[2][2] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
         }
         if(arr[0][2] == 1 && arr[1][1] == 1 && arr[2][0] == 1){
             x_wins = true;
+            System.out.println("X wins");
+            return true;
         }
-        if(arr[0][2] == 0 && arr[1][1] == 0 && arr[2][0] == 0){
+        if(arr[0][2] == -1 && arr[1][1] == -1 && arr[2][0] == -1){
             o_wins = true;
+            System.out.println("O wins");
+            return true;
+        }
+//
+//        if(x_wins) System.out.println("X wins");
+//        else if(o_wins) System.out.println("O wins");
+//        else if(total_moves != 9) System.out.println("Game not finished");
+//        else System.out.println("Draw");
+
+        if(total_moves == 9){
+            System.out.println("Draw");
+            return true;
         }
 
-        if(x_wins) System.out.println("X Wins");
-        else if(o_wins) System.out.println("O Wins");
-        else if(total_moves != 9) System.out.println("Game not finished");
-        else System.out.println("Draw");
+        return false;
     }
 }
